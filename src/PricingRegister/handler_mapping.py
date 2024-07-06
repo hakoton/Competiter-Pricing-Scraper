5cc30813bb6fcd338058d09dd53d652e6bb4e0ac
Riku Isayama <rrrikukawano@gmail.com>
2024-07-06 12:28:49 +0900

    feat:ラムダのハンドラマッピングを追加する

---
+++ src/PricingRegister/handler_mapping.py
@@ -0,0 +1,5 @@
+from registers import printpac_label_seal_reg
+
+FILE_PREFIX_MAP: dict = {
+    "printpac-label-seal": printpac_label_seal_reg,
+}
---
+++ src/PricingRegister/lambda_function.py
@@ -0,0 +1,54 @@
+import urllib.parse
+import boto3
+from typing import Dict
+
+from global_settings import GlobalSettings
+import handler_mapping
+
+
+def lambda_handler(event: Dict, context):
+    """
+    S3 イベント:
+    {
+        "Records": [
+            {
+                "s3": {
+                    "bucket": {
+                        "name": str
+                    }
+                    "object": {
+                        "key": str
+                    }
+                }
+            }
+         ]
+    }
+    """
+
+    # S3 Put eventから通知が来るため、対象のファイル名を取得
+    settings: GlobalSettings = GlobalSettings()
+    s3 = boto3.client("s3")
+    bucket: str = event["Records"][0]["s3"]["bucket"]["name"]
+
+    # 例:
+    # key    = pricing/printpac-label-seal_2024-06-23-00-22-25.json
+    # fname  = printpac-label-seal_2024-06-23-00-22-25.json
+    # target = printpac-label-seal
+    key: str = urllib.parse.unquote_plus(
+        event["Records"][0]["s3"]["object"]["key"], encoding="utf-8"
+    )
+    fname: str = key.split("/")[-1]
+    target: str = fname.split("_")[0]
+
+    try:
+        # ファイル名からInvoke先を特定し、ストリームを渡す
+        if settings.DEBUG_S3_NO_STREAM:
+            handler_mapping.FILE_PREFIX_MAP[target].doRegist(None)
+        else:
+            response = s3.get_object(Bucket=bucket, Key=key)
+            handler_mapping.FILE_PREFIX_MAP[target].doRegist(response["Body"].read())
+        return f"Pricing Register : {key} Succeed!"
+
+    except Exception as e:
+        print(e)
+        raise e
