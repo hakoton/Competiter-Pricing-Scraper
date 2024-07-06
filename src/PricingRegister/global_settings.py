c71a9abc19f684c62b936ba03fa45edf31cf8ebe
Riku Isayama <rrrikukawano@gmail.com>
2024-07-06 12:17:19 +0900

    feat: global_settingsをシングルトンとして追加

---
+++ src/PricingRegister/global_settings.py
@@ -0,0 +1,22 @@
+# Singleton
+class GlobalSettings:
+    _instance = None
+
+    AWS_REGION = "ap-northeast-1"
+    S3_PRICING_BUCKET_NAME = "mbs-for-test"
+    S3_PRICING_SUBDIR_PATH = "pricing/"
+    # AWS SSM関連
+    SSM_AUTH_PRM_KEY = "/mbs-new-pricing/auth/bigquery"
+    # BiqQuery関係
+
+    BQ_PROJECT_ID = "raksulcrm-dev"
+    BQ_DATASET_ID = "lake_competitor"
+    DEBUG_S3_NO_STREAM = False
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(GlobalSettings, cls).__new__(cls)
+            for key, value in kwargs.items():
+                setattr(cls._instance, key, value)
+
+        return cls._instance
